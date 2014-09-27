52e1fddcd278220a6e680bddf5324a50289fef00
Stuart Fyfe <stuart@motorroom.net>
2014-09-27 01:51:01 +0100

    Implemented spec and wrote some code

--- spec/BowlingGameSpec.php
+++ spec/BowlingGameSpec.php
@@ -19,6 +19,39 @@ class BowlingGameSpec extends ObjectBehavior
         $this->score()->shouldBe(0);
     }
 
+    function it_scores_twenty_if_all_single_pins()
+    {
+        $this->rollTimes(20, 1);
+
+        $this->score()->shouldBe(20);
+    }
+
+    function it_scores_a_perfect_game_as_three_hundred()
+    {
+        $this->rollTimes(12, 10);
+
+        $this->score()->shouldBe(300);
+    }
+
+    function it_scores_a_single_turkey_as_sixty()
+    {
+        $this->rollTimes(3, 10);
+        $this->rollTimes(14, 0);
+
+        $this->score()->shouldBe(60);
+    }
+
+    function it_scores_a_spare_plus_six_as_twenty()
+    {
+        $this->roll(6);
+        $this->roll(4);
+        $this->roll(4);
+        $this->roll(2);
+        $this->rollTimes(16, 0);
+
+        $this->score()->shouldBe(20);
+    }
+
     private function rollTimes($count, $pins)
     {
         for ($i = 0; $i < $count; $i++) {
--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -2,20 +2,72 @@
 
 class BowlingGame
 {
-    private $score;
+    const FRAMES_PER_GAME = 10;
 
-    public function __construct()
-    {
-        $this->score = 0;
-    }
+    private $rolls = [];
 
     public function roll($pins)
     {
-        $this->score += $pins;
+        $this->rolls[] = $pins;
     }
 
+    /**
+     * Calculate the score for the current game
+     *
+     * @return void
+     */
     public function score()
     {
-        return $this->score;
+        $score = 0;
+        $roll  = 0;
+
+        $frames = self::FRAMES_PER_GAME;
+
+        for ($frame = 1; $frame <= $frames; $frame++) {
+            $r           = $this->rolls;
+            if (count($r) === $roll) {
+                break;
+            }
+            $thisRoll    = $r[$roll];
+            $lastRoll    = (isset($r[$roll-1])) ? $r[$roll-1] : 0;
+            $twoRollsAgo = (isset($r[$roll-2])) ? $r[$roll-2] : 0;
+
+            // If last frame was a spare
+            if ($lastRoll + $twoRollsAgo === 10 && $lastRoll > 0 && $twoRollsAgo > 0) {
+                $score += $thisRoll;
+            }
+
+            if ($this->isStrike($lastRoll)) {
+                $score += 10;
+            }
+            if ($this->isStrike($twoRollsAgo)) {
+                $score += 10;
+            }
+
+            $score += $thisRoll;
+            if ($this->isStrike($thisRoll)) {
+                if ($roll+1 === self::FRAMES_PER_GAME) {
+                    $frames++;
+                }
+
+                $roll++;
+                continue;
+            }
+
+            $roll++;
+            if (count($r) === $roll) {
+                break;
+            }
+
+            $score += $r[$roll];
+            $roll++;
+        }
+
+        return $score;
+    }
+
+    protected function isStrike($pins)
+    {
+        return $pins === 10;
     }
 }
